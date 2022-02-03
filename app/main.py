27f0b63084431fcbe8e3a7b1ca9d7ccf49a78afe
unknown <rakkakeren@gmail.com>
2022-02-03 17:51:30 +0700

    add more display

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
 import streamlit as st
-from views import Header, Sidebar
+from views import InfoBoard, GAParam, Header
 
-
-header = Header()
-sidebar = Sidebar()
-
+# Header Section
+header = Header(
+    title="Prediksi Harga Emas", 
+    desc="Aplikasi untuk melakukan prediksi pada harga beli atau harga jual emas"
+)
 header.build()
-sidebar.build()
+st.markdown("---")
+
+
+# Content Section
+## Dataset Info
+model_info = InfoBoard(
+    title="Dataset", 
+    desc="Data yang digunakan adalah data harga beli emas dan harga jual emas pada tanggal xx sampai xx")
+model_info.build()
+
+
+## Model Info
+model_info = InfoBoard(
+    title="Informasi Model", 
+    desc="Model yang digunakan adalah regresi linier dan regresi linier dengan algoritma genetika")
+model_info.build()
+
+
+## Genetic Algorithm Parameters
+genparam = GAParam()
+user_input = genparam.build()
--- app/views.py
+++ app/views.py
@@ -1,4 +1,13 @@
+from abc import abstractmethod
+import json
 import streamlit as st
+
+
+class ViewElement:
+
+    @abstractmethod
+    def build(self):
+        ...
 
 
 class Component:
@@ -11,38 +20,60 @@ class Component:
         return self.comp(*self.args, **self.kwargs)
 
 
-class Header():
-    def __init__(self) -> None:
-        self.title = Component(st.title, body="Prediksi Harga Emas")
-        self.desc = Component(st.write, "Aplikasi untuk melakukan prediksi pada harga beli atau harga jual emas")
+class Header(ViewElement):
+    def __init__(self, title, desc) -> None:
+        self.title = Component(st.title, body=title)
+        self.desc = Component(st.write, desc)
         self.comps = [self.title, self.desc]
 
     def build(self) -> None:
         for comp in self.comps:
             comp.show()
-        
 
-class Sidebar:
+
+class InfoBoard(ViewElement):
+    def __init__(self, title, desc) -> None:
+        self.title = Component(st.subheader, body=title)
+        self.desc = Component(st.write, desc)
+        self.comps = [self.title, self.desc]
+
+    def build(self) -> None:
+        st.markdown("#")
+        for comp in self.comps:
+            comp.show()
+        st.markdown("---")
+
+
+class GAParam(ViewElement):
     def __init__(self) -> None:
-        self.title = Component(st.subheader, body="Parameter")
+        self.title = Component(st.subheader, body="Setelan Algoritma Genetika")
+        self.desc = Component(st.write, 
+        """Tentukan nilai parameter untuk melatih model 
+        menggunakan metode regresi linier dengan optimalisasi algoritma genetika.
+        Setelah parameter ditentukan, klik 'Latih'.
+        """
+        )
         self.generation = Component(st.number_input, label="Jumlah Generasi", min_value=1, max_value=100, step=10)
         self.size = Component(st.number_input, label="Ukuran Populasi", min_value=10, max_value=1500, step=100)
         self.cr = Component(st.number_input, label="Crossover Rate", min_value=0.0, max_value=1.0, step=0.1)
         self.mr = Component(st.number_input, label="Mutation Rate", min_value=0.0, max_value=1.0, step=0.1)
-        self.submit = Component(st.form_submit_button, label="Submit")
+        self.submit = Component(st.form_submit_button, label="Latih")
 
         self.comps = [self.generation, self.size, self.cr, self.mr]
-
+        self.pnames = ["generasi", "ukuran populasi", "crossover rate", "mutation rate"]
 
     def build(self) -> dict:
-        values = []
-        with st.sidebar.form("Parameter"):
-            self.title.show()
-            for comp in self.comps:
+        st.markdown("#")
+        self.title.show()
+        self.desc.show()
+
+        params = {}
+        with st.form("Parameter"):
+            for key, comp in zip(self.pnames, self.comps):
                 val = comp.show()
-                values.append(val)
+                params[key] = val
         
             is_submit = self.submit.show()
 
         if is_submit:
-            st.write(values)
+            return params
