7b361c79694291429c81886db68faf4c98ca99f6
unknown <rakkakeren@gmail.com>
2022-02-07 11:13:39 +0700

    add ga and linreg train

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from sklearn.linear_model import LinearRegression
 from views import InfoBoard, InfoBoardWithButton, GAParam, PreParam, Header
 from src.data import load_data
 from src.models import gen_algo
@@ -60,15 +61,34 @@ is_train = train_info.build()
 st.session_state["is_train"] = is_train
 
 if st.session_state.get("size") and is_train:
-    st.write("session")
+    # Latih Regresi Linier
+    st.write("Melatih regresi linier...")
+    linreg_beli = LinearRegression().fit(beli_train["X_train"], beli_train["y_train"])
+    linreg_jual = LinearRegression().fit(jual_train["X_train"], jual_train["y_train"])
+    st.write("Selesai")
+
+    # Latih Regresi Linier dengan GA
+    st.markdown("#")
+    st.write("Melatih regresi linier dengan GA...")
     ga_input = {
         "size": st.session_state.get("size"),
         "n_gen": st.session_state.get("n_gen"),
         "cr": st.session_state.get("cr"),
         "mr": st.session_state.get("mr"),
     }
-    ga_beli = gen_algo(**ga_input, **beli_train)
-    ga_jual = gen_algo(**ga_input, **beli_train)
+    population_beli, fitness_beli, linreg_beli_ga = gen_algo(**ga_input, **beli_train)
+    population_jual, fitness_jual, linreg_jual_ga = gen_algo(**ga_input, **beli_train)
+    st.write("Selesai")
+
+    # Simpan hasil train ke dalam session
+    st.session_state["populasi_beli"] = population_beli
+    st.session_state["populasi_jual"] = population_jual
+    st.session_state["fitness_beli"]  = fitness_beli
+    st.session_state["fitness_jual"]  = fitness_jual
+    st.session_state["linreg_beli_ga"]  = linreg_beli_ga
+    st.session_state["linreg_jual_ga"]  = linreg_jual_ga
+    st.session_state["linreg_beli"]  = linreg_beli
+    st.session_state["linreg_jual"]  = linreg_jual
 
 
-st.session_state
+# st.session_state
